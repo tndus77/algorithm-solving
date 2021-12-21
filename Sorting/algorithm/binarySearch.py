279704af9a03757767c5ab3a4e7f0951c4641b67
tndus77 <tndus77@sookmyung.ac.kr>
2021-12-21 03:47:34 +0000

    [FIX] 이진탐색 2개의 문제 풀이

--- Sorting/algorithm/binarySearch.py
+++ Sorting/algorithm/binarySearch.py
@@ -27,3 +27,17 @@ if result == None:
 else:
     #몇 번째 원소인지 출력
     print(result + 1)
+    
+#---------------
+def binary_search(array, target, start, end):
+    while(start <= end):
+        mid = (start + end) // 2
+        
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else :
+            start = mid + 1
+    return None
+
