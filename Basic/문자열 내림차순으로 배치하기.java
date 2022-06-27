e79f3b735b6c392dc1443e43c574a7566d6d90cd
tndus77 <tndus77@sookmyung.ac.kr>
2022-06-27 18:46:55 +0900

    [fix] 문자열 내림차순으로 배치하기

--- Basic/문자열 내림차순으로 배치하기.java
+++ Basic/문자열 내림차순으로 배치하기.java
@@ -8,7 +8,7 @@ class Solution {
         //각 문자를 잘라서 배열에 넣어주기
         String[] str = s.split("");
         
-        //오름차순으로 정렬
+        //내림차순으로 정렬
         Arrays.sort(str, Collections.reverseOrder());
         
         for(int i=0; i<str.length; i++) {
