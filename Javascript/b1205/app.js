8ca2f55486a0fb19b97d3c583db66984e6ed3ca5
sooyeon <tndus77@sookmyung.ac.kr>
2023-07-02 21:47:43 +0900

    1205 등수 구하기

---
+++ Javascript/b1205/app.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const filePath =
+	process.platform === 'linux' ? '/dev/stdin' : 'Javascript/b1205/input.txt';
+const input = fs.readFileSync(filePath).toString().split('\n');
+
+solution(input);
+
+function solution(input) {
+	const num = +input[0];
+
+	const scoreArr = input[1].split(' ').map((item) => +item);
+	const rankArr = Array.from({ length: num }, () => 1);
+	console.log(scoreArr);
+	console.log(rankArr);
+
+	for (let i = 0; i < num; i++) {
+		for (let j = 0; j < num; j++) {
+			if (scoreArr[i] < scoreArr[j]) {
+				rankArr[i]++;
+			}
+		}
+	}
+	console.log(rankArr);
+}
