b078e342675fa8ec5c65cdf3dbeb6d14ee010eb3
sooyeon <tndus77@sookmyung.ac.kr>
2023-07-02 21:31:08 +0900

    2506 점수 계산

---
+++ Javascript/b2506/app.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const filePath =
+	process.platform === 'linux' ? '/dev/stdin' : 'Javascript/b2506/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+console.log(solution(input));
+
+function solution(input) {
+	const num = +input[0];
+
+	const arr = input[1].split(' ');
+
+	let cnt = 0;
+	let answer = 0;
+
+	for (let i = 0; i < num - 1; i++) {
+		if (Number(arr[i]) === 1) {
+			cnt++;
+			answer += cnt;
+		} else cnt = 0;
+	}
+	return answer;
+}
