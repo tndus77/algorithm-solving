232440e99d363c3cc6bf89e1789a13f680ab218d
sooyeon <77282190+tndus77@users.noreply.github.com>
2023-11-04 02:57:51 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 368 ms, Memory: 54360 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const filePath =
+	process.platform === 'linux'
+		? '/dev/stdin'
+		: 'Javascript/softeer/구간합구하기4.txt';
+let input = fs.readFileSync(filePath).toString().trim();
+
+solution(input);
+
+function solution(input) {
+	const arr = input.split('\n')[1].split(' ').map(Number);
+	const sumArr = [0]; // 0부터 시작
+	const output = [];
+
+	arr.forEach((v, i) => {
+		sumArr[i + 1] = sumArr[i] + v; // sumArr에 arr 값을 하나씩 누적한다
+	});
+	let calculateArr = input.split('\n').slice(2);
+	for (let i = 0; i < calculateArr.length; i++) {
+		let [start, end] = calculateArr[i].split(' ');
+		output.push(sumArr[end] - sumArr[start - 1]);
+	}
+
+	console.log(output.join('\n'));
+}
