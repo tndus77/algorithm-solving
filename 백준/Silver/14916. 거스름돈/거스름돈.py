bc917c6c4314533fc6dea8e63698f067009dba9b
sooyeon <77282190+tndus77@users.noreply.github.com>
2024-01-15 23:26:17 +0900

    [Silver V] Title: 거스름돈, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+total = 0
+
+while True:
+  if n % 5 == 0:
+    total += n // 5
+    break
+  else:
+    n -= 2
+    total += 1
+
+  if n < 0:
+    break
+
+if n < 0:
+  print(-1)
+else:
+  print(total)
