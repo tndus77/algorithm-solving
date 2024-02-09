bb3e02bcb1b2626ebd2d1ef365e8cc164171c28b
sooyeon <77282190+tndus77@users.noreply.github.com>
2024-02-09 13:35:32 +0900

    [level 3] Title: 단속카메라, Time: 1.68 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,11 @@
+def solution(routes):
+    # 고속도로에 진입한 지점, 고속도로에서 나간 지점
+    answer = 0
+    routes.sort(key=lambda x:x[1])
+    camera = -30001
+    
+    for route in routes:
+        if camera < route[0]:
+            camera = route[1]
+            answer += 1
+    return answer
