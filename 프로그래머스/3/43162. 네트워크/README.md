7c23190f363596f48cd2642c90bbae152d73616f
sooyeon <77282190+tndus77@users.noreply.github.com>
2024-02-10 22:19:56 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162#qna) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.21 ms
+메모리: 10.2 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 14:25:2
+2024년 2월 6일 22:19:55
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,18 @@
+from collections import deque
 def solution(n, computers):
     answer = 0
-    m = len(computers[0])
-    v = [False for _ in range(n)]
+    visited = [False for _ in range(n)]
     
-    def DFS(x):
-        v[x] = True
+    def dfs(idx):
+        visited[idx] = True
         
-        for j in range(m):
-            if computers[x][j] == 1 and v[j] == False:
-                DFS(j)
-    
+        for i in range(n):
+            if not visited[i] and computers[idx][i]: # 방문 X & 연결되어있어
+                dfs(i)
     
     for i in range(n):
-        if v[i] == False:
-            DFS(i)
-            answer += 1
+        if not visited[i]:
+            dfs(i)
+            answer += 1 # dfs 끝나면 answer + 1
     return answer
 
