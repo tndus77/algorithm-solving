d0bbe63fed6d71285233ec4ae88e6c949e4b55e9
sooyeon <77282190+tndus77@users.noreply.github.com>
2024-08-18 21:55:16 +0900

    [level 3] Title: 네트워크, Time: 1.39 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,22 @@
-from collections import deque
+
 def solution(n, computers):
     answer = 0
-    visited = [False for _ in range(n)]
     
-    def dfs(idx):
-        visited[idx] = True
+    visited = [0] * n
+    
+    def dfs(N):
+        visited[N] = 1
         
         for i in range(n):
-            if not visited[i] and computers[idx][i]: # 방문 X & 연결되어있어
+            if N == i:
+                continue
+            if computers[N][i] == 1 and not visited[i]:
                 dfs(i)
+        
     
     for i in range(n):
         if not visited[i]:
             dfs(i)
-            answer += 1 # dfs 끝나면 answer + 1
-    return answer
+            answer += 1
 
+    return answer
