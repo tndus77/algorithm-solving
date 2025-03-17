d4dffad6d9f7db36972c56216598b8e3f23178c5
sooyeon <77282190+tndus77@users.noreply.github.com>
2025-03-17 16:12:48 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,26 +1,27 @@
-from collections import deque
 def solution(progresses, speeds):
-    rest_days = deque()
-    
+    # 작업 진도 progresses
+    # 작업 속도 speeds
+    # 조건 1: 뒤에 있는 기능이 먼저 개발되면 앞에 있는 기능이 개발될 때 같이 배포된다.
+    # 조건 2: 1일 1배포
+    total = []
+    answer = []    
     for i in range(len(progresses)):
-        day = 0
-        
-        need = 100 - progresses[i]
-        if need % speeds[i] == 0:
-            day = need // speeds[i]
+        rest = 0
+        if (100 - progresses[i]) % speeds[i] == 0:
+            # 나머지가 없으면
+            rest = (100 - progresses[i]) // speeds[i]
         else:
-            day = need // speeds[i] + 1
-        rest_days.append(day)
-        
-    res = []
+            rest = (100 - progresses[i]) // speeds[i] + 1
+        total.append(rest)
     
-    # 마지막 값보다 작으면 빼지 않고, 크면 빼고, cnt 초기화
-    while rest_days:
-        temp = rest_days.popleft()
-        cnt = 1
-        
-        while rest_days and temp >= rest_days[0]:
-            rest_days.popleft()
-            cnt += 1
-        res.append(cnt)
-    return res
+    pivot = total[0]
+    result = 1
+    for i in range(1, len(total)):
+        if pivot < total[i]:
+            answer.append(result)
+            pivot = total[i]
+            result = 1
+        else:
+            result += 1
+    answer.append(result)
+    return answer
