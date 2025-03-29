7c71562ce7380fe3adf051afacec8665de0737a8
sooyeon <77282190+tndus77@users.noreply.github.com>
2025-03-29 18:45:35 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,16 +1,20 @@
 def solution(n, computers):
     answer = 0
     visited = [False] * n
-
-    def dfs(idx):
-        for i in range(n):
-            if not visited[i] and computers[idx][i] == 1:
-                visited[i] = True
-                dfs(i)
     
-    for i in range(n):
-        if not visited[i]:
+    def dfs(x):
+        visited[x] = True
+        
+        if x == n:
+            return
+        
+        for i in range(n):
+            if not visited[i] and computers[x][i] == 1:
+                dfs(i)
+        
+    for i in range(len(computers)):
+        if not visited[i] and computers[i][i] == 1:
             dfs(i)
             answer += 1
-
+            
     return answer
