4f97a371fd724cda4e3f5dbdffaab05b06d16f8e
sooyeon <77282190+tndus77@users.noreply.github.com>
2025-05-15 23:36:09 +0900

    [level 2] Title: 할인 행사, Time: 162.76 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=python3) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.03 ms
+메모리: 16.2 MB, 시간: 162.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 12일 17:33:21
+2025년 05월 15일 23:36:04
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,20 +1,19 @@
+from collections import Counter
 def solution(want, number, discount):
-    from collections import Counter
-
-    answer = 0
-    days = 10
     want_dict = dict(zip(want, number))
+    answer = 0
     
-    for start_day in range(len(discount) - days+1):
-        current_items = discount[start_day:start_day+days]
-        current_counter = Counter(current_items)
+    for i in range(len(discount)-9):
+        window = discount[i:i+10]
+        window_counter = Counter(window)
+        match = True
         
-        isPossible = True
-        for item, count in want_dict.items():
-            if current_counter[item] < count:
-                isPossible = False
-        if isPossible:
+        for item in want_dict:
+            if  window_counter[item] < want_dict[item]:
+                match = False
+                break
+                
+        if match:
             answer += 1
-    
-    
-    return answer
+    return answer        
+        
