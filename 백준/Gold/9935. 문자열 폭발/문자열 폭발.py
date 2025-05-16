be935ac2bcdca667d49041505e768e19cd2ee8af
sooyeon <77282190+tndus77@users.noreply.github.com>
2025-05-16 18:18:43 +0900

    [Gold IV] Title: 문자열 폭발, Time: 488 ms, Memory: 43596 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,16 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 s = input().strip()
 bomb = input().strip()
 stack = []
 
 for char in s:
   stack.append(char)
-  if len(stack) >= len(bomb):
+  if char == bomb[-1] and len(stack) >= len(bomb):
     if ''.join(stack[-len(bomb):]) == bomb:
       for _ in range(len(bomb)):
         stack.pop()
 
 result = ''.join(stack)
-if result:
-  print(result)
-else:
-  print("FRULA")
+print(result if result else 'FRULA')
