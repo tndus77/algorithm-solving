b3de538d51835e6b57aed81796edf847f1e1e465
sooyeon <77282190+tndus77@users.noreply.github.com>
2025-05-22 17:40:21 +0900

    [Silver I] Title: 스타트와 링크, Time: 2224 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,23 +1,33 @@
-import sys
-from itertools import combinations
-
 N = int(input())
+M = N//2
 arr = [list(map(int, input().split())) for _ in range(N)]
-groups = [x for x in range(N)]
-groups = list(combinations(groups, N//2))
-ans = 1e9
+ans = 100*M*M
 
-for group in groups:
-  team_A = 0
-  team_B = 0
-  for i in group:
-    for j in group:
-      team_A += arr[i][j]
-  not_team = [x for x in range(N) if x not in group]
-  for i in not_team:
-    for j in not_team:
-      team_B += arr[i][j]
-  
-  ans = min(ans, abs(team_A - team_B))
+def cal(alst, blst):
+  asum = bsum = 0
 
+  for i in range(M):
+    for j in range(M):
+      asum += arr[alst[i]][alst[j]]
+      bsum += arr[blst[i]][blst[j]]
+
+  return abs(asum - bsum)
+
+def dfs(n, start, link):
+  global ans
+
+  if len(start) > M or len(link) > M:
+    return
+    
+  if n == N:
+    if len(start) == len(link) == M:
+      ans = min(ans, cal(start, link))
+    return
+
+  # 스타트팀
+  dfs(n+1, start+[n], link)
+  # 링크팀
+  dfs(n+1, start, link+[n])
+
+dfs(0, [], [])
 print(ans)
