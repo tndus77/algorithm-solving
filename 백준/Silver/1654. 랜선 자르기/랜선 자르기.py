7e1d9482d7146644dd2f62b578efff5c6609942e
sooyeon <77282190+tndus77@users.noreply.github.com>
2025-06-04 22:09:21 +0900

    [Silver II] Title: 랜선 자르기, Time: 364 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,27 +1,24 @@
 import sys
 
-# 이미 가지고 있는 랜선의 개수, 필요한 랜선의 개수
 K, N = map(int, input().split())
-lst = [int(input()) for _ in range(K)]
-start, end = 0, max(lst)
+lines = [int(input()) for _ in range(K)]
+
+start, end = 1, max(lines)
+result = 0
 
 while start <= end:
-  mid = (start + end) // 2
-  sum = 0
-
-  for num in lst:
-    if mid == 0:
-      sum += num
+    answer = 0
+    mid = (start + end) // 2
+    
+    for i in range(K):
+        if (lines[i] // mid) > 0:
+            answer += lines[i] // mid
+    
+    if answer >= N:
+        # 남아돈다!
+        result = mid # 가능한 길이 중 하나, 더 길게 시도!
+        start = mid + 1
     else:
-      sum += num // mid # 현재 길이에서 얻을 수 있는 랜선 총 갯수
-  
-  if N > sum:
-    # 랜선을 더 가져야해!
-    # 상한가를 낮춘다.
-    end = mid - 1
-  else:
-    # 랜선 너무 많이 가졌어
-    # 상한가를 더 높게 가진다.
-    start = mid + 1
-  
-print(end)
+        # 더 잘게 잘라야한다.
+        end = mid - 1
+print(result)
